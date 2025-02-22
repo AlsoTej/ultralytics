33fd267d1baaafc68a23c4a4beb2d5541249844f
AlsoTej <tejas.sharma10@gmail.com>
2025-02-22 23:35:43 +0530

    Update tasks.py
    Signed-off-by: AlsoTej <tejas.sharma10@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -63,6 +63,9 @@ from ultralytics.nn.modules import (
     TorchVision,
     WorldDetect,
     v10Detect,
+    BiFPN,
+    BiFPNBlock,
+    
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
