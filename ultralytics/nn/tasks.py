fc6258014c1b4f2d75fff9f3b67d74457bd8fb60
AlsoTej <tejas.sharma10@gmail.com>
2025-03-19 00:00:15 +0530

    Update tasks.py
    Signed-off-by: AlsoTej <tejas.sharma10@gmail.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1127,9 +1127,9 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [c1, c2 // 16, *args[2:]]
         elif m is CoordAtt:  # Handle CoordAtt separately
             args = [c1, c2]  # c1 = input channels, c2 = output channels
-            if len(from_) > 1:  # If multiple inputs are concatenated
-                c1 = sum([ch[f] for f in from_])  # Sum channels from all sources
-            m_ = m(*args)  # Instantiate CoordAtt            
+            if isinstance(f, list) and len(f) > 1:  # If multiple inputs are concatenated
+                c1 = sum([ch[i] for i in f])  # Sum channels from all sources
+            m_ = m(*args)  # Instantiate CoordAtt        
         else:
             c2 = ch[f]
      
